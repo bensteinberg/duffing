2e98daf35627fdcaceda98f85a8511c9d32090ef
Ben Steinberg <bsteinberg@law.harvard.edu>
2019-08-16 13:35:03 -0400

    Blacken python file

--- duffing.py
+++ duffing.py
@@ -17,23 +17,25 @@ import struct
 def f1(t, x, v):
     return v
 
+
 # vdot
 def f2(t, x, v, alpha, beta, delta, gamma, omega):
-    return gamma * math.cos(omega * t) - delta * v - alpha * x - beta * x**3
+    return gamma * math.cos(omega * t) - delta * v - alpha * x - beta * x ** 3
+
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--alpha', type=float, help='stiffness',     default=1.0)
-    parser.add_argument('--beta', type=float,  help='non-linearity', default=5.0)
-    parser.add_argument('--delta', type=float, help='damping',       default=0.02)
-    parser.add_argument('--gamma', type=float, help='amplitude',     default=8.0)
-    parser.add_argument('--omega', type=float, help='frequency',     default=0.5)
+    parser.add_argument("--alpha", type=float, help="stiffness", default=1.0)
+    parser.add_argument("--beta", type=float, help="non-linearity", default=5.0)
+    parser.add_argument("--delta", type=float, help="damping", default=0.02)
+    parser.add_argument("--gamma", type=float, help="amplitude", default=8.0)
+    parser.add_argument("--omega", type=float, help="frequency", default=0.5)
 
-    parser.add_argument('--time',  type=float, help='total time', default=1000.0)
-    parser.add_argument('--step',  type=float, help='step size',  default=0.01)
+    parser.add_argument("--time", type=float, help="total time", default=1000.0)
+    parser.add_argument("--step", type=float, help="step size", default=0.01)
 
-    parser.add_argument('-x',  type=float, help='initial position', default=0.0)
-    parser.add_argument('-v',  type=float, help='initial velocity', default=0.0)
+    parser.add_argument("-x", type=float, help="initial position", default=0.0)
+    parser.add_argument("-v", type=float, help="initial velocity", default=0.0)
 
     # alpha = 1.0
     # beta = 5.0
@@ -49,7 +51,7 @@ def main():
     omega = args.omega
 
     tt = args.time
-    h  = args.step
+    h = args.step
 
     # initial conditions
     x = args.x
@@ -58,17 +60,21 @@ def main():
 
     output = []
 
-    while (t < tt):
+    while t < tt:
         k1x = f1(t, x, v)
         k1v = f2(t, x, v, alpha, beta, delta, gamma, omega)
-        k2x = h * f1(t + h/2, x + k1x/2, v + k1v/2)
-        k2v = h * f2(t + h/2, x + k1x/2, v + k1v/2, alpha, beta, delta, gamma, omega)
-        k3x = h * f1(t + h/2, x + k2x/2, v + k2v/2)
-        k3v = h * f2(t + h/2, x + k2x/2, v + k2v/2, alpha, beta, delta, gamma, omega)
+        k2x = h * f1(t + h / 2, x + k1x / 2, v + k1v / 2)
+        k2v = h * f2(
+            t + h / 2, x + k1x / 2, v + k1v / 2, alpha, beta, delta, gamma, omega
+        )
+        k3x = h * f1(t + h / 2, x + k2x / 2, v + k2v / 2)
+        k3v = h * f2(
+            t + h / 2, x + k2x / 2, v + k2v / 2, alpha, beta, delta, gamma, omega
+        )
         k4x = h * f1(t + h, x + k3x, v + k3v)
         k4v = h * f2(t + h, x + k3x, v + k3v, alpha, beta, delta, gamma, omega)
-        x = x + h * (k1x + 2*k2x + 2*k3x + k4x) / 6
-        v = v + h * (k1v + 2*k2v + 2*k3v + k4v) / 6
+        x = x + h * (k1x + 2 * k2x + 2 * k3x + k4x) / 6
+        v = v + h * (k1v + 2 * k2v + 2 * k3v + k4v) / 6
         print("%f %f %f" % (t, x, v))
         output.append(x)
         t = t + h
@@ -83,5 +89,6 @@ def main():
     #     f.writeframesraw( data )
     # f.close()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
